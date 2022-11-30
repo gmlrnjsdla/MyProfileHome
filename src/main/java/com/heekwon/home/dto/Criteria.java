8c68d5ee4f0dff00a4b3cabfed7fc826dfbad15d
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-11-30 13:43:06 +0900

    게시판 페이징 기능 구현 완료. / 복습 必

--- src/main/java/com/heekwon/home/dto/Criteria.java
+++ src/main/java/com/heekwon/home/dto/Criteria.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class Criteria {
 	
 	private int pageNum = 1; 	//현재 페이지 번호
-	private int amount = 5; 	//한 페이지에 출력 표시될 글의 수
+	private int amount = 6; 	//한 페이지에 출력 표시될 글의 수
 	private int startNum; 		//현재 선택된 페이지에서 시작할 글 번호
 	
 	public String getQuerryString() {	//페이지 파라미터 값을 넘김
